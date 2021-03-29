39e897f0d78afb9b3f0b96c72e1eaf2fac17fd0b
huutai507 <huutai050798@gmail.com>
2021-03-29 16:57:58 +0700

    update api key

--- src/libs/giphy.js
+++ src/libs/giphy.js
@@ -1,3 +1,3 @@
 import { GiphyFetch } from "@giphy/js-fetch-api";
 
-export const giphy = new GiphyFetch(process.env.REACT_APP_GIPHY);
+export const giphy = new GiphyFetch("e3XijBRN98RxgdFW36l7h4ODJsU57geL");
